01d80465be0423300fce5bfa12016b67d6250f37
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-04-14 15:04:44 +0530

    delete record api with action

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+    return (
+      <p id="index-page">
+        This is a demo for Remix.
+        <br />
+        Check out my {" "}
+        <a href="https://github.com/shubhamwagh2544">GitHub profile</a>.
+      </p>
+    );
+  }
+  
